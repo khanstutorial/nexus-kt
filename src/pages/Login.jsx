ab1bb103cb1f0843353d3ca2d18abab8e1b71c22
Syed Saqib Hasanie <syedsaqibhasanie@MACs-MacBook-Pro.local>
2022-05-29 23:00:53 +0500

    login page created

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Button, Grid } from '@mui/material';
+
+const Login = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3}>
+        <Button variant="contained" href="http://auth.ktemail.com/">
+          Login with KeyCloak
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,25 @@
-
-import React from 'react'
-import {  Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Welcome from '../pages/Welcome';
 import Status from '../pages/Status';
+import Login from '../pages/Login';
+import Layout from '../layout/statusLayout';
+
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Status />} />
-        <Route path='/welcome' element={<Welcome />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Status />
+          </Layout>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/welcome" element={<Welcome />} />
     </Routes>
-    )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
