b891a174cd4b8d0b4a800162d9c9bcd4a2ae8323
Syed Saqib Hasanie <syedsaqibhasanie@MACs-MacBook-Pro.local>
2022-05-30 03:28:41 +0500

    started working on profile page

---
+++ src/features/Custom-Input/CustomInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField,
+  TextFieldProps,
+  Typography,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+const CustomInput = () => {
+  return <div>CustomInput</div>;
+};
+
+export default CustomInput;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Grid, Card, Box, Typography } from '@mui/material';
+import Container from '@mui/material/Container';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          width: '100%',
+          marginTop: '10%',
+          paddingBottom: '20px',
+          textAlign: 'center',
+          minHeight: '100%',
+          backgroundColor: 'white',
+        }}
+      >
+        <Typography variant="h5">Personal Profile</Typography>
+      </Box>
+      <Card sx={{ padding: '35px' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <span>First Name</span>
+          </Grid>
+          <Grid item xs={6}>
+            <span>Last Name</span>
+          </Grid>
+          <Grid item xs={6}>
+            <span>Email Address</span>
+          </Grid>
+          <Grid item xs={6}>
+            <span>Phone Number</span>
+          </Grid>
+        </Grid>
+      </Card>
+    </Container>
+  );
 };
 
 export default Profile;
