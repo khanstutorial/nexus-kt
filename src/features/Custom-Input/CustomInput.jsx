0ee1eecf6c190b0fc62e4a319846dd8c27703ce8
Syed Saqib Hasanie <syedsaqibhasanie@macs-mbp.home>
2022-06-05 21:45:33 +0500

    profile page completed

--- src/features/Custom-Input/CustomInput.jsx
+++ src/features/Custom-Input/CustomInput.jsx
@@ -10,8 +10,50 @@ import {
 } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
-const CustomInput = () => {
-  return <div>CustomInput</div>;
+const CustomInput = ({
+  label,
+  value,
+  id,
+  name,
+  placeholder,
+  type = 'text',
+  isDisable = false,
+}) => {
+  return (
+    <Stack spacing={0.75}>
+      {label && (
+        <Typography
+          variant="body1"
+          color={'blue'}
+          fontWeight="fontWeightMedium"
+        >
+          {label}
+        </Typography>
+      )}
+      {isDisable ? (
+        <Box height={'48px'} display="flex" alignItems="center">
+          <Typography
+            variant="body1"
+            color={'grey'}
+            fontWeight="fontWeightMedium"
+          >
+            {value}
+          </Typography>
+        </Box>
+      ) : (
+        <>
+          <TextField
+            maxRows={4}
+            id={id}
+            multiline
+            name={name}
+            value={value}
+            type={type}
+          />
+        </>
+      )}
+    </Stack>
+  );
 };
 
 export default CustomInput;
