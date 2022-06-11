7c80ea8bf2ff704e97599f277793842a0af5ee4b
musaraza008 <76042013+musaraza008@users.noreply.github.com>
2022-06-11 23:48:05 +0500

    fix main layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import { Typography, Box } from '@mui/material'
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+import KT from './../../assets/images/KT.svg';
+import { Grid } from '@mui/material';
+import favicon from './../../assets/images/favicon.png';
 
-const Header = (props) => {
+const Header = props => {
   return (
-    <Box sx={{marginTop:'20px'}}>
-      <Typography variant='h6' sx={{  paddingRight:'2px', float:'left' }}>
-        <b>{props.name}</b>
-      </Typography>
-      <Typography  variant='h6' color={'blue'}>{props.content}</Typography>
-    </Box>
-  )
-}
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="flex-end"
+      direction={'row'}
+      sx={{ margin: 0 }}
+    >
+      <Grid
+        item
+        sx={{ border: '1px solid red' }}
+        justifyContent="flex-start"
+        md={8}
+        xs={1}
+      >
+        <img src={favicon}></img>
+      </Grid>
+      <Grid item xs={1}>
+        <Typography variant="h6" color={'blue'}>
+          Section 1
+        </Typography>{' '}
+      </Grid>
+      <Grid item xs={1}>
+        <Typography variant="h6" color={'blue'}>
+          Section 2
+        </Typography>
+      </Grid>
+      <Grid item xs={1}>
+        <Typography variant="h6" color={'blue'}>
+          Section 3
+        </Typography>
+      </Grid>
+      <Grid item xs={1}>
+        <Typography variant="h6" color={'blue'}>
+          Login
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/layout/layout.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { Grid } from '@mui/material';
+import Container from '@mui/material/Container';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import KTLogoPNG from './../assets/images/KTLogoPNG.png';
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <Box
+      minHeight="100%"
+      position={'relative'}
+      style={{ border: '1px solid', margin: 0 }}
+    >
+      <Header name={'KT '} content={'system status'} />
+      <Box
+        sx={{
+          width: '100%',
+          paddingTop: '5px',
+          paddingBottom: '20px',
+          minHeight: '100px',
+          backgroundColor: '#F5F5F5',
+        }}
+      >
+        {children}
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default DefaultLayout;
