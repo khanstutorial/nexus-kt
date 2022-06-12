482a34f8dfcd79aeb74f5de823420d32b4eee37b
saqibs98 <saqibs186@gmail.com>
2022-06-13 04:58:48 +0500

    Popup code optimised

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,23 +1,42 @@
 const SERVICES = [
-    {
-        id: 'API',
-        value: 'API',
-        label: 'API',
-      },
-      {
-        id: 'Dashboard',
-        value: 'Dashboard',
-        label: 'Dashboard',
-    },
-    {
-        id: 'Stripe',
-        value: 'Stripe',
-        label: 'Stripe',
-      }, {
-        id: 'Checkout',
-        value: 'Checkout',
-        label: 'Checkout',
-      },
-]
+ {
+  id: 'API',
+  value: 'API',
+  label: 'API',
+ },
+ {
+  id: 'Dashboard',
+  value: 'Dashboard',
+  label: 'Dashboard',
+ },
+ {
+  id: 'Stripe',
+  value: 'Stripe',
+  label: 'Stripe',
+ },
+ {
+  id: 'Checkout',
+  value: 'Checkout',
+  label: 'Checkout',
+ },
+];
 
-export default SERVICES;
+const SECTIONS = [
+ {
+  id: 'Section-1',
+  name: 'Section 1',
+  options: ['AOption 1', 'Option 2', 'Option 3'],
+ },
+ {
+  id: 'Section-2',
+  name: 'Section 2',
+  options: ['BOption 1', 'Option 2', 'Option 3'],
+ },
+ {
+  id: 'Section-3',
+  name: 'Section 3',
+  options: ['COption 1', 'Option 2', 'Option 3'],
+ },
+];
+
+export { SERVICES, SECTIONS };
--- src/pages/Status.jsx
+++ src/pages/Status.jsx
@@ -5,51 +5,67 @@ import Stack from '@mui/material/Stack';
 import { Card } from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
 import { Typography } from '@mui/material';
-import SERVICES from './../constants/index';
+import { SERVICES } from './../constants/index';
 import UptimeGraphBar from '../components/Uptime-Graph-Bar/UptimeGraphBar';
 
 const Status = () => {
-  let date = moment().utcOffset('-4').format('DD MMMM, YYYY hh:mm');
+    let date = moment().utcOffset('-4').format('DD MMMM, YYYY hh:mm');
 
-  const data = [
-    1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0,
-    1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0,
-  ];
+    const data = [
+        1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+        0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+        1, 0,
+    ];
 
-  return (
-    <Card sx={{ padding: '0px !important' }}>
-      <Box sx={{ minHeight: '155px', padding: '0 36px' }}>
-        <Box sx={{ padding: '50px 0px' }}>
-          <CircleIcon
-            sx={{ color: '#2cfa2c', paddingRight: '22px', float: 'left' }}
-          />
-          <Box sx={{ paddingLeft: '24px' }}>
-            <Typography variant="h5" component="h2">
-              All services are online.
-            </Typography>
-            <Typography mt={2} sx={{ paddingLeft: '22px' }} variant="subtitle1">
-              As of {date}
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
-      <Stack
-        spacing={2}
-        sx={{ backgroundColor: '#f2f8fb', padding: '35px', minHeight: '400px' }}
-      >
-        {SERVICES.map(service => (
-          <>
-            <span key={service}>{service.value}</span>
-            <div key={service} sx={{ display: 'flex', flexDirection: 'row' }}>
-              {data.map(number => (
-                <UptimeGraphBar value={number} />
-              ))}
-            </div>
-          </>
-        ))}
-      </Stack>
-    </Card>
-  );
+    return (
+        <Card sx={{ padding: '0px !important' }}>
+            <Box sx={{ minHeight: '155px', padding: '0 36px' }}>
+                <Box sx={{ padding: '50px 0px' }}>
+                    <CircleIcon
+                        sx={{
+                            color: '#2cfa2c',
+                            paddingRight: '22px',
+                            float: 'left',
+                        }}
+                    />
+                    <Box sx={{ paddingLeft: '24px' }}>
+                        <Typography variant="h5" component="h2">
+                            All services are online.
+                        </Typography>
+                        <Typography
+                            mt={2}
+                            sx={{ paddingLeft: '22px' }}
+                            variant="subtitle1"
+                        >
+                            As of {date}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+            <Stack
+                spacing={2}
+                sx={{
+                    backgroundColor: '#f2f8fb',
+                    padding: '35px',
+                    minHeight: '400px',
+                }}
+            >
+                {SERVICES.map(service => (
+                    <>
+                        <span key={service}>{service.value}</span>
+                        <div
+                            key={service}
+                            sx={{ display: 'flex', flexDirection: 'row' }}
+                        >
+                            {data.map(number => (
+                                <UptimeGraphBar value={number} />
+                            ))}
+                        </div>
+                    </>
+                ))}
+            </Stack>
+        </Card>
+    );
 };
 
 export default Status;
