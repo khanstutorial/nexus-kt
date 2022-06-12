66a158e5d5fba0ff779ade9f01cfb0b75769c08d
musaraza008 <76042013+musaraza008@users.noreply.github.com>
2022-06-13 02:44:46 +0500

    navbar popup completed

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import KTLogo from '../assets/images/svgs/KTLogo';
 const Welcome = () => {
-  return (
-    <>
-      <KTLogo></KTLogo>
-      {/* <img src={KT}></img> */}
-    </>
-  );
+  return <>{/* <img src={KT}></img> */}</>;
 };
 
 export default Welcome;
