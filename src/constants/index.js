fad80b42b3ce817a1517a737d8650f2d7d66e3ec
musaraza008 <76042013+musaraza008@users.noreply.github.com>
2022-06-13 04:18:42 +0500

    navigation page UI done

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,17 +25,17 @@ const SECTIONS = [
  {
   id: 'Section-1',
   name: 'Section 1',
-  options: ['AOption 1', 'Option 2', 'Option 3'],
+  options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
  },
  {
   id: 'Section-2',
   name: 'Section 2',
-  options: ['BOption 1', 'Option 2', 'Option 3'],
+  options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
  },
  {
   id: 'Section-3',
   name: 'Section 3',
-  options: ['COption 1', 'Option 2', 'Option 3'],
+  options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
  },
 ];
 
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import KTLogo from '../assets/images/svgs/KTLogo';
+import { Typography } from '@mui/material';
 const Welcome = () => {
-  return <>{/* <img src={KT}></img> */}</>;
+  return (
+    <>
+      <Typography sx={{ padding: '50px' }}>
+        I recommend for the navigation bar at the top of your page, use the font
+        we use everywhere (at least I try to get people to use it as much as I
+        can -- I am only one person), which is Poppins. Stick with the bold
+        version of the typeface. Bold black text on plain white is your best bet
+        of people actually paying attention to what’s in front of them. Follow
+        the grey login button but if you really don’t like it, use our carolina
+        blue color. HEX code: #58a8dc You should also do hover animations like
+        transitioning color or something to just make people feel good about
+        clicking (which is the end goal). Always have a 0.4s transision on the
+        animations with ease-all animation timing (at least thats the term for
+        html css code -- idk what you’re using).
+      </Typography>
+    </>
+  );
 };
 
 export default Welcome;
